ed632f59bf47f62730ce92b960e7c37dfe61a905
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-01-10 10:47:08 +0100

    Finished Board.java

---
+++ src/Board.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+class Board {
+    private ArrayList<Player> playersList;
+    private ArrayList<Card> currentlyPlayingCards;
+    private ArrayList<Card> drawedCards;
+
+    public Board() {
+        playersList = new playersList();
+    }
+
+    public ArrayList<Player> getPlayersList() {
+        return playersList;
+    }
+
+    public void giveCardsToWinner(Player winner) {
+        for(int i = 0; i < playersList.size(); i++) {
+            if(winner == playersList.get(i)) {
+                if(currentlyPlayingCards.size() > 0) {
+                    playersList.get(i).addListOfCardsToBottom(currentlyPlayingCards);
+                    currentlyPlayingCards.clear();
+                }
+                if(drawedCards.size() > 0) {
+                    playersList.get(i).addListOfCardsToBottom(drawedCards);
+                    drawedCards.clear();
+                }
+            }
+        }
+    }
+
+    public boolean checkIfNoOneWon() {
+        for(int i = 0; i < playersList.size(); i++) {
+            if(playersList.get(i).checkIfLost()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
