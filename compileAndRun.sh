c1f5b8b4ad695daeb199a25da4c4c8c5a45620f5
Ziemowit Błażków <yasha2yashaa@gmail.com>
2019-01-10 09:56:38 +0100

    Improved compileAndRun bash script

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-find src -name '*.java' > sources.list
-find out -name '*.class' > classes.list
-rm -f @classes.list
-javac @sources.list -d out && java -cp out
+mkdir out && rm -f out/*.class && javac -d out src/Main.java && java -cp out/ Main
